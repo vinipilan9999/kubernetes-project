d9bc18044535df941bb0d1ec978edf0f524db010
root <root@Windows10.opus.local>
2022-12-19 14:23:21 -0300

    Initial commit

---
+++ ingress-controller-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+# kubectl patch daemonsets -n projectcontour envoy -p '{"spec":{"template":{"spec":{"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
